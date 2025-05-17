0c50fbc676b5c84a97d2d4940511e4fbbdc6d4e0
Lakshaya kumar <lakshaya.3009.kr@gmail.com>
2025-05-17 06:43:47 +0000

    convesion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "lalu"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueinNumber  =  Number(score)
+//console.log(typeof valueinNumber)
+//console.log(valueinNumber)
+
+let number ="hello ji"
+let boolIsloggedIn = Boolean(number)
+// console.log(boolIsloggedIn);
+
+// 1 => true
+// "" => false
+// "Helloji " => true
+
+let num = 34
+console.log(typeof num);
+
+let convertToString = String(num)
+console.log(convertToString);
+
+console.log(typeof convertToString);
+
