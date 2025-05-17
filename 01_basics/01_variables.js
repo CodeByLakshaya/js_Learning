d88cd6fb779c94ee2f4c1c58e92253625c5a1909
Lakshaya kumar <lakshaya.3009.kr@gmail.com>
2025-05-17 06:02:32 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 122331
+let accountEmail = "Rohan@gmail.com"
+var accountPassword = "12345"
+accountCity = "delhi"
+//accountId = 345
+accountEmail = "lalu@gmail.com"
+accountPassword = "67890"
+accountCity = "dawarka"
+console.table([accountEmail, accountId, accountPassword, accountCity])
